cdf2da427fd5dc65395a2e065dceb628a4855a70
Fred Parke <fjcparke@gmail.com>
2019-02-02 15:23:36 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/unit-cards/precache-manifest.69fa6d2bd6eeab926ad4150e4f14c4f2.js"
+  "/unit-cards/precache-manifest.2ed82eaceaab65b43a5baa267c2a25e6.js"
 );
 
 workbox.clientsClaim();
